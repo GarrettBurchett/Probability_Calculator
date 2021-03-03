b70654d0e843296c8407f2d537748fdfb11a54c6
gtburchett <gburchett88@gmail.com>
2021-03-02 23:17:24 -0600

    Added some code to Hat class and experiment function. Added gitignore file.

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,18 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.contents = []
+        for k, v in self.kwargs.items():
+            for i in range(kwargs[k]):
+                self.contents.append(i)
+    
+    def draw(self, number_of_draws):
+        self.number_of_draws = number_of_draws
+        self.balls_drawn = []
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    trials = 1
+    while trials <= num_experiments:
+        hat.draw(num_balls_drawn)
